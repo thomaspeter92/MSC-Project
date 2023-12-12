55bfdf1b0202c4ecb22244f5ba2d0c911b91d654
Thomas Buckley <thomasbuckley@Thomass-MacBook-Pro.local>
2023-12-12 16:29:42 +0900

    switch to supabase for development

--- frontend/src/stores/userStore.tsx
+++ frontend/src/stores/userStore.tsx
@@ -2,19 +2,17 @@ import { create } from "zustand";
 import { getUserToken, signIn } from "../services/firebaseService";
 
 type UserStore = {
-  signIn: (email: string, password: string) => void
-}
+  signIn: (email: string, password: string) => void;
+};
 
 export const useUserStore = create<UserStore>()((set, get) => ({
   loggedIn: false,
 
   signIn: async (email: string, password: string) => {
     try {
-      await signIn(email, password)
-      let token = await getUserToken()
-      console.log(token)
-    }catch(error){
-
-    }
-  }
-}))
+      await signIn(email, password);
+      let token = await getUserToken();
+      console.log(token);
+    } catch (error) {}
+  },
+}));
